0a6d63a4821b4dabf1212c02df3b27c7bb245e88
Shawon_codex <blackrisebd@gmail.com>
2025-01-08 21:24:00 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Menubar from "@/components/Menubar/Menubar";
-import Footer from "@/components/Footer/Footer";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Menubar />
+       
         {children}
-        <Footer />
+       
       </body>
     </html>
   );
