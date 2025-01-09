6fc70bc6a301a59be521ffbe0294299e534d2539
Shawon_codex <blackrisebd@gmail.com>
2025-01-09 23:40:16 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata = {
-  title: "BlackRise - Creative Web Design & Development Services",
+  title: "20 + login website design professonal Next Js Script",
   description:
     "BlackRise is a web and software development agency specializing in creative, high-performance websites and custom software solutions, helping businesses grow with innovative digital experiences.",
 };
